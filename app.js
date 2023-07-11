0230c4011e26cf3fdbfa527737a6d33036a74777
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-07-11 22:29:59 +0400

    linkedtree clone

---
+++ app.js
@@ -0,0 +1,15 @@
+const shareButtons = document.querySelectorAll('.tile-share')
+
+async function copyText(e) {
+    e.preventDefault()
+    const link = this.getAttribute('link')
+
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied to clipboard: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButtons => shareButtons.addEventListener('click', copyText))
